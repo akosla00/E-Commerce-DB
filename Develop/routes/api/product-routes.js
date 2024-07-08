585c8ab538c1b35a1bd41cbb9c39c6347f7a6d80
akosla00 <akosla00@gmail.com>
2024-07-08 18:54:47 -0400

    completed routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -18,9 +18,17 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const categoryData = await Product.findByPk(req.params.id, {
+      include: [{ model: Tag, through: ProductTag, as: 'product_price' }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -100,8 +108,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const categoryData = await Product.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
